b8f7cfb10106ecce0fe0e567d2539b9e72dd22e0
snehamishra <sneha.mishra@bestandless.com.au>
2023-09-10 17:49:37 +1000

    Update executeCommerceBuild.groovy

--- vars/executeCommerceBuild.groovy
+++ vars/executeCommerceBuild.groovy
@@ -1,6 +1,15 @@
 def call(commerceDir) {
-	echo "##### Execute install script ##### -- ${commerceDir}"
-	addProperty(commerceDir, "solrserver.instances.default.autostart=false")
-	sh "cd ${commerceDir}/core-customize/hybris/bin/platform && . ./setantenv.sh && ant clean all"
+	echo "##### Goto spartacus folder #####"
+	sh "cd ${commerceDir}/js-storefront/spartacus-ssr"
+	echo "##### Install yarn #####"
+	sh "yarn install"
+	echo "##### Execute build #####"
+	sh "yarn run build:ssr"
+	echo "##### Commit dist folder #####"
+	sh "git commit dist && git push"
+	
+	
+	//addProperty(commerceDir, "solrserver.instances.default.autostart=false")
+	//sh "cd ${commerceDir}/core-customize/hybris/bin/platform && . ./setantenv.sh && ant clean all"
 } 
    
