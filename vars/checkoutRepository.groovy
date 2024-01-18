e7d675d3f5820c3cde7e32dd5f829eb813c9e51b
Vishnu <vishnu.burra@bestandless.com.au>
2024-01-18 15:46:02 +1100

    Update checkoutRepository.groovy

--- vars/checkoutRepository.groovy
+++ vars/checkoutRepository.groovy
@@ -1,6 +1,6 @@
 def call(commerceDir, branch, projectRepository) {
     urlPrefix = "https://"
-    withCredentials([[$class: 'SshUserPrivateKeyBinding', credentialsId: 'VishnuBitBucket', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {        
+    withCredentials([[$class: 'SSHUserPrivateKeyBinding', credentialsId: 'VishnuBitBucket', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {        
         repoDomainPart = projectRepository.substring(urlPrefix.size())
         repository = "https://$USERNAME:$PASSWORD@" + repoDomainPart
         echo "##### Checkout repository #####"
