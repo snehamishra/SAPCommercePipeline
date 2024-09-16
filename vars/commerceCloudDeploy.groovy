bc9d4d2928a8b65b8399d5c022941164ad04717d
Abhishek Singhai <abhishek.singhai@bestandless.com.au>
2024-09-16 11:10:09 +0530

    revert s1 check only

--- vars/commerceCloudDeploy.groovy
+++ vars/commerceCloudDeploy.groovy
@@ -2,15 +2,12 @@ def call(buildName, dbUpdateMode, environmentId, strategy) {
     echo "##### Initiate Deployment to SAP Commerce Cloud Environment #####"
     //deploy tag 
     script{
-        if("s1".equals(environmentId)) {           
-            withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'commerceCloudCredentials', usernameVariable: 'subscriptionId', passwordVariable: 'token']]) {
+        withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'commerceCloudCredentials', usernameVariable: 'subscriptionId', passwordVariable: 'token']]) {
             deploy = sh (script: "curl --location --request POST 'https://portalrotapi.hana.ondemand.com/v2/subscriptions/${subscriptionId}/deployments' --header 'Content-Type: application/json' --header 'Authorization: Bearer ${token}' --header 'Content-Type: text/plain' --data-raw '{\"buildCode\": \"${buildName}\",\"databaseUpdateMode\": \"${dbUpdateMode}\",\"environmentCode\": \"${environmentId}\",\"strategy\": \"${strategy}\"}'",returnStdout:true)
             echo "$deploy"
             deploy_result = readJSON text: "$deploy"
             deploy_code = deploy_result["code"]
-            return deploy_code     
-            }
-            echo "deploy S1 build only"
+            return deploy_code
         }
     }
 }
