b836065a8e2e59ee79003520624bbf8db139e639
Abhishek Singhai <abhishek.singhai@bestandless.com.au>
2026-03-10 06:30:29 +0530

    HB-11110: change the pipelines portal urls

--- vars/buildCommerceCloudCheck.groovy
+++ vars/buildCommerceCloudCheck.groovy
@@ -2,7 +2,7 @@ def call(codeNumber) {
     script {
         while (true) {
           withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'commerceCloudCredentials', usernameVariable: 'subscriptionId', passwordVariable: 'token']]) {
-              result = sh (script: "curl --location --request GET 'https://portalrotapi.hana.ondemand.com/v2/subscriptions/${subscriptionId}/builds/$codeNumber' --header 'Authorization: Bearer ${token}'",returnStdout:true)
+              result = sh (script: "curl --location --request GET 'https://portalapi.commerce.ondemand.com/v2/subscriptions/${subscriptionId}/builds/$codeNumber' --header 'Authorization: Bearer ${token}'",returnStdout:true)
           }
           echo "$result"
           statusResult = readJSON text: "$result"
